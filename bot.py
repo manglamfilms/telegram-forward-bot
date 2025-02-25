55272cac0605532e2dca7d956b9970b4c6b1abfc
manglamfilms <gra888038@gmail.com>
2025-02-25 18:59:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.filters import Command
 
-BOT_TOKEN = "7414662064:AAEauJcIDlFoQGaJlLBheABK_iuB0RMkP88"
-DESTINATION_CHAT_ID = "7198083451"  # जहाँ मैसेज फॉरवर्ड होगा
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+DESTINATION_CHAT_ID = "DESTINATION_CHAT_ID"  # जहाँ मैसेज फॉरवर्ड होगा
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(commands=['start'])
-async def start_command(message: types.Message):
+@dp.message(Command("start"))
+async def start_command(message: Message):
     await message.reply("Send me a message, and I'll forward it!")
 
 @dp.message()
-async def forward_message(message: types.Message):
+async def forward_message(message: Message):
     await bot.forward_message(DESTINATION_CHAT_ID, message.chat.id, message.message_id)
 
 async def main():
